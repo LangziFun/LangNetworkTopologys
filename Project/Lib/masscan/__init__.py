f7884036b65528f2db2491a1b44a040915d9e5f7
LangziFun <982722261@qq.com>
2020-01-27 13:55:01 +0800

    祝大家2020年平平安安

---
+++ Project/Lib/masscan/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Init file for the masscan bindings."""
+from .masscan import *
+
+__author__ = 'MyKings (xsseroot@gmail.com)'
+__version__ = '0.1.4'
+__last_modification__ = '2019.09.02'
+
